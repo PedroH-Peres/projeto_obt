14657a8e17310ca34de076266c0ea89b21c7417c
Pedro Henrique B. Peres <pedrohperescode@gmail.com>
2023-02-09 11:00:02 -0300

    AuthService

---
+++ lib/core/auth/auth_firebase_service.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:projeto_obt/core/auth/auth_service.dart';
+import 'package:projeto_obt/core/models/app_user.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class AuthFirebaseService implements AuthService{
+  static AppUser? _currentUser;
+  static final _userStream = Stream<AppUser?>.multi((controller) async { 
+    final authChanges = FirebaseAuth.instance.authStateChanges();
+    await for(final user in authChanges){
+      _currentUser = user == null ?null : _toAppUser(user);
+      controller.add(_currentUser);
+    }
+  });
+
+  @override
+  AppUser? get currentUser {
+    return _currentUser;
+  }
+
+  @override
+  Stream<AppUser?> get userChanges{
+    return _userStream;
+  }
+
+  @override
+  Future<void> login(String email, String password) async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> logout() async {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Future<void> signup(String name, String email, String password, String tipoConta) async {
+    final signup = await Firebase.initializeApp(
+      name: 'userSignup',
+      options: Firebase.app().options
+    );
+
+    final auth = FirebaseAuth.instanceFor(app: signup);
+
+    UserCredential credential = await auth.createUserWithEmailAndPassword(email: email, password: password);
+
+    if(credential.user != null){
+      await credential.user?.updateDisplayName(name);
+      await login(email, password);
+      _currentUser = _toAppUser(credential.user!, name, tipoConta);
+    }
+
+
+
+  }
+
+  static AppUser _toAppUser(User user, [String? name, String? tipoConta]) {
+    return AppUser(
+      id: user.uid,
+      name: name ?? user.displayName ?? user.email!.split('@')[0],
+      email: user.email!,
+      tipoConta: tipoConta ?? 'Aluno',
+    );
+  }
+
+}
---
+++ lib/core/auth/auth_service.dart
@@ -0,0 +1,17 @@
+import '../models/app_user.dart';
+
+abstract class AuthService{
+  AppUser? get currentUser;
+
+  Stream<AppUser?> get userChanges;
+
+  Future<void> signup(String name, String email, String password, String tipoConta);
+
+  Future<void> login (String email, String password);
+
+  Future<void> logout();
+
+  //factory AuthService(){return ;}
+
+
+}
